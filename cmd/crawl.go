ffd18a0135fce9bfde4aea9fa75ad5090968a4e9
AdriKusuma <adriananda06@gmail.com>
2026-02-02 11:42:54 +0800

    update for dir crawler

--- cmd/crawl.go
+++ cmd/crawl.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/gocolly/colly"
 	"github.com/spf13/cobra"
+	"github.com/fatih/color"
 )
 
 var crawl string
@@ -11,18 +13,12 @@ var crawlCmd = &cobra.Command{
 	Use:   "crawl",
 	Short: "Input target URL for crawling",
 	Run: func(cmd *cobra.Command, args []string) {
-
-		if crawl == "" {
-			fmt.Println("Empty URL, Use -h or --help for help")
-			return
-		}
-		fmt.Println("Target:", crawl)
+		RunCrawler(crawl)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(crawlCmd)
-
 	crawlCmd.Flags().StringVarP(
 		&crawl,
 		"url",
@@ -30,6 +26,37 @@ func init() {
 		"",
 		"Target URL",
 	)
-
 	crawlCmd.MarkFlagRequired("url")
 }
+
+func RunCrawler(target string) {
+	if target == "" {
+		fmt.Println("Empty URL")
+		return
+	}
+
+	c := colly.NewCollector()
+
+	c.OnResponse(func(r *colly.Response) {
+		if r.StatusCode == 200{
+			green := color.New(color.FgGreen).SprintFunc()
+			fmt.Println(green(r.StatusCode), r.Request.URL)
+		}else if r.StatusCode == 302 {
+			yellow := color.New(color.FgYellow).SprintFunc()
+			fmt.Println(yellow(r.StatusCode), r.Request.URL)
+		}else{
+			red:= color.New(color.FgRed).SprintFunc()
+			fmt.Println(red(r.StatusCode), r.Request.URL)
+		}
+		
+	})
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) { 
+		e.Request.Visit(e.Attr("href")) 
+	})
+
+	c.Visit(target)
+}
+
+
+
